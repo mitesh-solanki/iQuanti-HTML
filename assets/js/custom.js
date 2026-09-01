0ee9e68095cdbe5bc7989734bd92939f951ed28e
namratatrivedi2310 <namrata@prismitsystems.com>
2026-09-01 18:20:35 +0530

    Solutions section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -79,3 +79,101 @@ jQuery(function ($) {
     });
 
 });
+
+// Slider js //
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const sliderTrack = document.querySelector(".research-track");
+    const sliderItems = document.querySelectorAll(".research-slide");
+    const previousButton = document.querySelector(".research-prev");
+    const nextButton = document.querySelector(".research-next");
+    const paginationDots = document.querySelectorAll(".research-dot");
+
+    let activeSlide = 0;
+    let autoPlay;
+
+
+    function showSlide(index) {
+
+        if (index >= sliderItems.length) {
+            activeSlide = 0;
+        } else if (index < 0) {
+            activeSlide = sliderItems.length - 1;
+        } else {
+            activeSlide = index;
+        }
+
+        sliderTrack.style.transform =
+            "translateX(-" + (activeSlide * 100) + "%)";
+
+        paginationDots.forEach(function (dot, index) {
+            dot.classList.toggle(
+                "active",
+                index === activeSlide
+            );
+        });
+    }
+
+
+    function nextSlide() {
+        showSlide(activeSlide + 1);
+    }
+
+
+    function previousSlide() {
+        showSlide(activeSlide - 1);
+    }
+
+
+    nextButton.addEventListener("click", function () {
+        nextSlide();
+        restartAutoPlay();
+    });
+
+
+    previousButton.addEventListener("click", function () {
+        previousSlide();
+        restartAutoPlay();
+    });
+
+
+    paginationDots.forEach(function (dot, index) {
+
+        dot.addEventListener("click", function () {
+            showSlide(index);
+            restartAutoPlay();
+        });
+
+    });
+
+
+    function startAutoPlay() {
+        autoPlay = setInterval(function () {
+            nextSlide();
+        }, 5000);
+    }
+
+
+    function stopAutoPlay() {
+        clearInterval(autoPlay);
+    }
+
+
+    function restartAutoPlay() {
+        stopAutoPlay();
+        startAutoPlay();
+    }
+
+
+    const sliderArea = document.querySelector(".research-slider");
+
+    sliderArea.addEventListener("mouseenter", stopAutoPlay);
+    sliderArea.addEventListener("mouseleave", startAutoPlay);
+
+
+    /* Initial slide */
+    showSlide(0);
+    startAutoPlay();
+
+});
